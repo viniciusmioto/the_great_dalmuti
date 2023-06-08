a3389396de012c505cb488c2b2a4304fee89e2f0
viniciusmioto <vinemioto@gmail.com>
2023-06-08 19:01:33 -0300

    feat: get current and next machines' info

--- src/network.py
+++ src/network.py
@@ -1,45 +1,91 @@
 import socket
 
 
-def read_machine_configurations(file_path):
-    machine_configs = {}
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
-        for i in range(0, len(lines), 4):
-            machine_name = lines[i].strip()
-            machine_ip = lines[i + 1].split(': ')[1].strip()
-            machine_port = int(lines[i + 2].split(': ')[1].strip())
-            machine_configs[machine_name] = (machine_ip, machine_port)
-    return machine_configs
+def get_next_machine(config_file, current_machine):
+    machines = {}
+
+    with open(config_file, "r") as file:
+        lines = file.readlines()
+        num_machines = int(lines[0].split()[1])
+
+        for i in range(1, len(lines)):
+            line = lines[i].strip()
+
+            if line.startswith("MACHINE"):
+                machine_number = int(line.split()[1])
+                machine_info = {"number": machine_number}
+            elif line.startswith(("ADDRESS", "SEND_ADDRESS", "SEND_PORT", "RECV_PORT")):
+                key, value = line.split()
+                machine_info[key.split("_")[0].lower()] = value
+
+                if line.startswith("RECV_PORT"):
+                    machines[machine_number] = machine_info
+
+    if current_machine in machines:
+        current_machine_info = machines[current_machine]
+        next_machine_number = (current_machine % num_machines) + 1
+        next_machine_info = machines[next_machine_number]
+
+        next_machine_info["number"] = next_machine_number
+        return next_machine_info
+
+    return None
+
+
+def get_current_machine(config_file, current_machine):
+    machines = {}
+
+    with open(config_file, "r") as file:
+        lines = file.readlines()
+        num_machines = int(lines[0].split()[1])
+
+        for i in range(1, len(lines)):
+            line = lines[i].strip()
+
+            if line.startswith("MACHINE"):
+                machine_number = int(line.split()[1])
+                machine_info = {"number": machine_number}
+            elif line.startswith(("ADDRESS", "SEND_ADDRESS", "SEND_PORT", "RECV_PORT")):
+                key, value = line.split()
+                machine_info[key.split("_")[0].lower()] = value
+
+                if line.startswith("RECV_PORT"):
+                    machines[machine_number] = machine_info
+
+    if current_machine in machines:
+        return machines[current_machine]
+
+    return None
+
+
+
+
+def receive_message(receiver_ip, receiver_port):
+    # Create a UDP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Bind the socket to the receiver's IP and port
+    sock.bind((receiver_ip, receiver_port))
+
+    # Receive the message
+    data, addr = sock.recvfrom(1024)
+    message = data.decode()
+
+    # Close the socket
+    sock.close()
+
+    return message
 
 
 def send_message(sender_ip, sender_port, receiver_ip, receiver_port, message):
+    # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sender_address = (sender_ip, sender_port)
-    receiver_address = (receiver_ip, receiver_port)
-    sock.bind(sender_address)
-    sock.sendto(message.encode(), receiver_address)
+
+    # Bind the socket to the sender's IP and port
+    sock.bind((sender_ip, sender_port))
+
+    # Send the message to the receiver
+    sock.sendto(message.encode(), (receiver_ip, receiver_port))
+
+    # Close the socket
     sock.close()
-
-
-def receive_message(receiver_ip, receiver_port, buffer_size=1024):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    receiver_address = (receiver_ip, receiver_port)
-    sock.bind(receiver_address)
-    data, address = sock.recvfrom(buffer_size)
-    sock.close()
-    return data.decode()
-
-
-def main():
-    machine_configs = read_machine_configurations('config.txt')
-    machine1_ip, machine1_port = machine_configs['Machine 1']
-    machine2_ip, machine2_port = machine_configs['Machine 2']
-    message = "Hello, receiver!"
-    send_message(machine1_ip, machine1_port, machine2_ip, machine2_port, message)
-    response = receive_message(machine2_ip, machine2_port)
-    print("Received:", response)
-
-
-if __name__ == '__main__':
-    main()
